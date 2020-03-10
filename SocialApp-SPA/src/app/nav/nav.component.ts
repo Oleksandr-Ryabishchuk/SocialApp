9490a63ebd07cb64aa3e7579f723657d6bf401cb
ryabishcuk2 <ryabishcuk2@gmail.com>
2020-03-10 22:33:12 +0200

    starting to apply 3d parties libraries

--- SocialApp-SPA/src/app/nav/nav.component.ts
+++ SocialApp-SPA/src/app/nav/nav.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
+import { AlertifyService } from '../_services/alertify.service';
 
 @Component({
   selector: 'app-nav',
@@ -8,16 +9,16 @@ import { AuthService } from '../_services/auth.service';
 })
 export class NavComponent implements OnInit {
  model: any = {};
-  constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService, private alertify: AlertifyService) { }
 
   ngOnInit() {
   }
 
   login() {
     this.authService.login(this.model).subscribe( next => {
-      console.log('Logged successfuly');
+      this.alertify.success('Logged successfuly');
     }, error => {
-      console.log(error);
+      this.alertify.error(error);
     });
   }
 
@@ -28,7 +29,7 @@ export class NavComponent implements OnInit {
 
   logOut() {
     localStorage.removeItem('token');
-    console.log('Logged Out');
+    this.alertify.message('Logged Out');
   }
 
 }
