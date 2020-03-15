4501b59099c402fadeeec6677d8dec4f09332df8
ryabishcuk2 <ryabishcuk2@gmail.com>
2020-03-15 15:57:35 +0200

    Add 3d party libraryes to make app more attractful

--- SocialApp-SPA/src/app/_services/auth.service.ts
+++ SocialApp-SPA/src/app/_services/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {map} from 'rxjs/operators';
+import {JwtHelperService} from '@auth0/angular-jwt';
 
 @Injectable({
   providedIn: 'root'
@@ -9,6 +10,9 @@ export class AuthService {
 
 constructor(private http: HttpClient) { }
     baseUrl = 'http://localhost:5000/api/auth/';
+    jwtHelper = new JwtHelperService();
+    decodedToken: any;
+
 
 login(model: any) {
   return this.http.post(this.baseUrl + 'login', model)
@@ -17,6 +21,8 @@ login(model: any) {
       const user = response;
       if (user) {
         localStorage.setItem('token', user.token);
+        this.decodedToken = this.jwtHelper.decodeToken(user.token);
+        console.log(this.decodedToken);
       }
     })
   );
@@ -24,5 +30,9 @@ login(model: any) {
 
 register(model: any) {
   return this.http.post(this.baseUrl + 'register', model);
+}
+loggedIn() {
+  const token = localStorage.getItem('token');
+  return !this.jwtHelper.isTokenExpired(token);
 }
 }
