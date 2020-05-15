e7dca76c9724fb833f0530080d49473f0334fd56
ryabishcuk2 <ryabishcuk2@gmail.com>
2020-05-16 00:33:55 +0300

    Paging, Sorting and Filtering

--- SocialApp-SPA/src/app/app.module.ts
+++ SocialApp-SPA/src/app/app.module.ts
@@ -13,7 +13,7 @@ import { AuthService } from './_services/auth.service';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
 import { ErrorInterceptorProvider } from './_services/error.interceptor';
-import { BsDropdownModule, TabsModule, BsDatepickerModule } from 'ngx-bootstrap';
+import { BsDropdownModule, TabsModule, BsDatepickerModule, PaginationModule, ButtonsModule } from 'ngx-bootstrap';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MemberListComponent } from './members/member-list/member-list.component';
 import { ListsComponent } from './lists/lists.component';
@@ -69,7 +69,9 @@ export class CustomHammerConfig extends HammerGestureConfig {
       BrowserAnimationsModule,
       BsDropdownModule.forRoot(),
       BsDatepickerModule.forRoot(),
+      PaginationModule.forRoot(),
       TabsModule.forRoot(),
+      ButtonsModule.forRoot(),
       RouterModule.forRoot(appRoutes),
       NgxGalleryModule,
       FileUploadModule,
--- SocialApp.API/Repositories/SocialRepository.cs
+++ SocialApp.API/Repositories/SocialRepository.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using SocialApp.API.Data;
+using SocialApp.API.Helpers;
 using SocialApp.API.InterfaceRepositories;
 using SocialApp.API.Models;
 using System;
@@ -44,11 +45,37 @@ namespace SocialApp.API.Repositories
             return user;
         }
 
-        public async Task<IEnumerable<User>> GetUsers()
+        public async Task<PagedList<User>> GetUsers(UserParams userParams)
         {
-            var users = await _context.Users.Include(p => p.Photos).ToListAsync();
-            return users;
+            var users =  _context.Users.Include(p => p.Photos)
+            .OrderByDescending(u => u.LastActive).AsQueryable();
+
+            users = users.Where(u => u.UserId != userParams.UserId && u.Gender == userParams.Gender);
+
+            if(userParams.MinAge != 18 || userParams.MaxAge != 99) 
+            {
+                var minDob = DateTime.Today.AddYears(-userParams.MaxAge -1);
+                var maxDob = DateTime.Today.AddYears(-userParams.MinAge);
+                users = users.Where(u => u.DateOfBirth >= minDob && u.DateOfBirth <= maxDob);
+            }
+
+            if(!string.IsNullOrEmpty(userParams.OrderBy))
+            {
+                switch(userParams.OrderBy) 
+                {
+                    case "created":
+                        users = users.OrderByDescending(u => u.Created);
+                        break;
+                        default:
+                        users = users.OrderByDescending(u => u.LastActive);
+                        break;
+                }
+
+            }
+
+            return await PagedList<User>.CreateAsync(users, userParams.PageNumber, userParams.PageSize);
         }
+
 
         public async Task<bool> SaveAll()
         {
